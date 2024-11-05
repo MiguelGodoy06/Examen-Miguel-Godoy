01ba843f7423364bfaa4dd3c128b9d6b573b3c31
MiguelGodoy06 <miguelgodoyavila1@gmail.com>
2024-11-05 11:38:41 +0100

    PARTE 1

--- PARTE 1/main1.cpp
+++ PARTE 1/main1.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int main() {
 int numero;
     cout<<"Ingrese un número: ";
-    cin>>numero;
+    cin>>numero;  //Le pedimos un numero y lo que nos de lo guardamos en nuestra variable tipo int numero.
 
-    if (numero!=0) {
+    if (numero!=0) { //Primero digo que si el número es distinto de 0, hay que ver si es positivo o negativo
         if(numero>0) {
-            cout<<"El número ingresado "<<numero<<" es positivo."<<endl;
+            cout<<"El número ingresado "<<numero<<" es positivo."<<endl; //Este if es para ver si es positivo
         }
         if(numero<0) {
-            cout<<"El número ingresado "<<numero<<" es negativo."<<endl;
+            cout<<"El número ingresado "<<numero<<" es negativo."<<endl;  //Este if es para ver si es negativo
         }
     }
-else {
+else {  //Esto quiere decir que si no es cualquier número que no sea 0, quiere decir que el número introducido es 0.
     cout<<"El número ingresado "<<numero<<endl;
 }
 
