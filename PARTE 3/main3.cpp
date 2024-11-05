d7b815b2d024647343928542d1f4b1c252b9ea5b
MiguelGodoy06 <miguelgodoyavila1@gmail.com>
2024-11-05 11:38:51 +0100

    PARTE 2

--- PARTE 3/main3.cpp
+++ PARTE 3/main3.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-    const int NumEjs = 8;
-    float notas[NumEjs];
-    float suma = 0.0;
+    const int NumEjs = 8; //Creo una constante para que no varíe el número de ejercicios
+    float notas[NumEjs];  //Creo un array para poder almacenar las notas
+    float suma = 0.0;   //Creo una variable para tener en cuenta la suma de todas las notas y poder sacar luego el promedio
 
-    for (int i = 0; i < NumEjs; i++) {
+    for (int i = 0; i < NumEjs; i++) {  //Empleo un ciclo for que pregunte las notas de cada ejercicio y las vaya almacenando a su vez que va haciendo la suma para luego sacar el promedio.
         cout << "\nIngrese la nota " << (i + 1) << ": ";
         cin >> notas[i];
         suma += notas[i];
     }
 
-    float promedio = suma / NumEjs;
+    float promedio = suma / NumEjs;  //Una vez tengo la suma de todos los ejercicios, lo divido por el propio número de ejercicios para poder sacar el promedio.
     cout << "\nLa nota media de los " << NumEjs << " ejercicios es de: " << promedio << endl;
 
 
